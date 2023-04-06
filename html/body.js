c64b885333b4beb6cf9e6fe043bf5b8eb95740c7
Langkiong <129997031+Langkiong@users.noreply.github.com>
2023-04-07 02:21:43 +0700

    Update body.js

--- html/body.js
+++ html/body.js
@@ -1,1 +1,19 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
 
+//Paste Iklan Disini
+
+		atOptions = {
+		'key' : '0dd90fc348cb5df9bf168dd5dd553063',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/0dd90fc348cb5df9bf168dd5dd553063/invoke.js"></scr' + 'ipt>');
+
+
+// Batas Iklan
+
+document.write('</div>');
+document.write('</div>');
