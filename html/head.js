ed42a07939a7c4f1c2b85bd5e24f46cc173a4869
Langkiong <129997031+Langkiong@users.noreply.github.com>
2025-08-25 17:03:56 +0700

    Update head.js

--- html/head.js
+++ html/head.js
@@ -1,1 +1,16 @@
-document.write('<img src="//sstatic1.histats.com/0.gif?4756655&101"/>');
+<script type="text/javascript">
+document.write("<!-- Histats.com START (async) -->\n");
+document.write("<script type='text/javascript'>\n");
+document.write("var _Hasync= _Hasync|| [];\n");
+document.write("_Hasync.push(['Histats.start', '1,4972483,4,0,0,0,00010000']);\n");
+document.write("_Hasync.push(['Histats.fasi', '1']);\n");
+document.write("_Hasync.push(['Histats.track_hits', '']);\n");
+document.write("(function() {\n");
+document.write("var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;\n");
+document.write("hs.src = ('//s10.histats.com/js15_as.js');\n");
+document.write("(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);\n");
+document.write("})();\n");
+document.write("<\/script>\n"); // note: escape closing script tag
+document.write("<noscript><a href='/' target='_blank'><img src='//sstatic1.histats.com/0.gif?4972483&101' alt='' border='0'></a></noscript>\n");
+document.write("<!-- Histats.com END -->\n");
+</script>
