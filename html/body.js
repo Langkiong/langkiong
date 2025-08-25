dce50edf5c9b6a73e137ae611299c80cdf96e556
Langkiong <129997031+Langkiong@users.noreply.github.com>
2025-08-25 14:38:16 +0700

    Update body.js

--- html/body.js
+++ html/body.js
@@ -1,21 +1,18 @@
-document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
-document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
+<div id="floatads" style="width:100%; margin:auto; text-align:center; position:fixed; bottom:0; z-index:9999; overflow:hidden;">
+  <div style="text-align:center; display:block; max-width:729px; height:auto; margin:auto; overflow:hidden;">
+    
+    <!-- Paste Iklan Disini -->
+    <script type="text/javascript">
+      atOptions = {
+        'key' : 'a647892478cfe688a80178b308049a3e',
+        'format' : 'iframe',
+        'height' : 90,
+        'width' : 728,
+        'params' : {}
+      };
+    </script>
+    <script type="text/javascript" src="//recessionhillside.com/a647892478cfe688a80178b308049a3e/invoke.js"></script>
+    <!-- Batas Iklan -->
 
-//Paste Iklan Disini
-
-		&lt;script type=&quot;text/javascript&quot;&gt;
-	atOptions = {
-		&#039;key&#039; : &#039;a647892478cfe688a80178b308049a3e&#039;,
-		&#039;format&#039; : &#039;iframe&#039;,
-		&#039;height&#039; : 90,
-		&#039;width&#039; : 728,
-		&#039;params&#039; : {}
-	};
-&lt;/script&gt;
-&lt;script type=&quot;text/javascript&quot; src=&quot;//recessionhillside.com/a647892478cfe688a80178b308049a3e/invoke.js&quot;&gt;&lt;/script&gt;
-
-
-// Batas Iklan
-
-document.write('</div>');
-document.write('</div>');
+  </div>
+</div>
