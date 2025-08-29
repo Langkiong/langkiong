2baf78bdb0fd7a0e55699532dec1da303df4f625
Langkiong <129997031+Langkiong@users.noreply.github.com>
2025-08-29 08:31:06 +0700

    Update imake.sh

--- imake.sh
+++ imake.sh
@@ -1,7 +1,7 @@
 apt update
-apt install php php-curl php-sqlite3 zip unzip tmux -y
+apt install php php-curl php-sqlite3 zip unzip tmux wget -y
 cd /var/www/html
 rm -rf index.html
-wget langkiong.my.id/imake.zip
+wget https://github.com/Langkiong/langkiong/raw/main/imake.zip -O imake.zip
 unzip imake.zip
 rm -rf imake.zip
